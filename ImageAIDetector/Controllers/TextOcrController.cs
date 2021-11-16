d9645740e71b9ac2141d8b2a8b9eba90eb263670
iwaitu <iwaitucn@gmail.com>
2021-11-17 00:04:42 +0800

    add ocr

---
+++ ImageAIDetector/Controllers/TextOcrController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TesseractOcr;
+
+namespace ImageAIDetector.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextOcrController : ControllerBase
+    {
+        [HttpPost("readtext")]
+        public async Task<string> ReadText(IFormFile file)
+        {
+            if (file.Length > 0)
+            {
+                var filePath = Path.GetTempFileName();
+
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    await file.CopyToAsync(stream);
+                    return OcrDetector.ReadText(stream);
+
+                }
+                
+            }
+            return await Task.FromResult("");
+        }
+    }
+}
---
+++ TesseractOcr/OcrDetector.cs
@@ -0,0 +1,22 @@
+﻿using Patagames.Ocr;
+using System.Drawing;
+
+namespace TesseractOcr
+{
+    public static class OcrDetector
+    {
+        public static string ReadText(Stream stream)
+        {
+            var target = (Bitmap)Image.FromStream(stream);
+            using (var objOcr = OcrApi.Create())
+            {
+                objOcr.Init(Patagames.Ocr.Enums.Languages.ChineseSimplified);
+
+                string plainText = objOcr.GetTextFromImage(target);
+
+                return plainText;
+            }
+        }
+
+    }
+}
